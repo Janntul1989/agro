7323b361f9ab13bd5157bfbef5dbc8c4af2282e5
Jannatul1989 <jchaiti72@gmail.com>
2023-08-08 19:50:02 +0600

    first commit

---
+++ app/Http/Controllers/frontend/contactcontroller.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class contactontroller extends Controller
+{
+    public function contact ()
+    {
+        
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use app\Http\Controllers\frontend\homecontroller;
+
+
+
+use app\Http\Controllers\frontend\aboutcontroller;
+
+use app\Http\Controllers\frontend\contactcontroller;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view(' frontend.Home');
+});
+
+
+Route::get('/', function () {
+    return view(' frontend.about');
+});
+
+Route::get('/', function () {
+    return view(' frontend.contact');
+});
+
+
+
